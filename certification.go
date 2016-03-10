57941007480cf77cf8da8de731fb04d0ce6b910c
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-10 15:19:18 +0200

    updates

--- certification.go
+++ certification.go
@@ -22,4 +22,26 @@ func readFile(filename string, password string) (*x509.Certificate, *rsa.Private
 	}
 
 	// Verify the certification
+	_, err = cert.Verify(x509.VerifyOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	switch e := err.(type) {
+	case x509.CertificateInvalidError:
+		switch e.Reason {
+		case x509.Expired:
+			return nil, nil, err
+		default:
+		}
+	case x509.UnknownAuthorityError:
+		return nil, nil, err
+	default:
+	}
+
+	priv, b := privateKey.(*rsa.PrivateKey)
+	if !b {
+		return nil, nil, fmt.Errorf("Error with private key")
+	}
+	return cert, priv, nil
 }

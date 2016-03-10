f944825bb20c01de6a8757d937bd8053da844412
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-10 10:41:18 +0200

    updates

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io/ioutil"
+
+	"golang.org/x/crypto/pkcs12"
 )
 
 func main() {
@@ -17,4 +22,46 @@ func main() {
 	}
 
 	// decode the .p12 file
+	privateKey, cert, err := pkcs12.Decode(p12, password)
+
+	if err != nil {
+		fmt.Errorf("Can't decode %v", err)
+	}
+
+	// check if certificate is valid
+
+	v, err := cert.Verify(x509.VerifyOptions{})
+
+	switch e := err.(type) {
+	case x509.CertificateInvalidError:
+		switch e.Reason {
+		case x509.Expired:
+			fmt.Println("expired")
+		default:
+			fmt.Errorf("error %v", err)
+		}
+	case x509.UnknownAuthorityError:
+	default:
+	}
+
+	fmt.Println("v: ", v)
+
+	priv, ok := privateKey.(*rsa.PrivateKey)
+
+	if !ok {
+		fmt.Errorf("error %v", ok)
+	}
+
+	fmt.Println("priv: ", priv)
+
+	t := tls.Certificate{
+		Certificate: [][]byte{cert.Raw},
+		PrivateKey:  privateKey,
+		Leaf:        cert,
+	}
+
+	fmt.Println("t: ", t)
+
+	// create new client
+
 }

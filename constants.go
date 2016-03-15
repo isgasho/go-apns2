2dc5b968257e7342538b15762131684419b44a45
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-15 17:15:01 +0200

    adding headers

---
+++ constants.go
@@ -0,0 +1,8 @@
+package apns2
+
+const (
+	ApnsID         = "apns-id"
+	ApnsExpiration = "apns-expiration"
+	ApnsPriority   = "apns-priority"
+	ApnsTopic      = "apns-topic"
+)
---
+++ headers.go
@@ -0,0 +1,22 @@
+package apns2
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// Header request headers for apple push notification
+type Header struct {
+	ID          string
+	Expiration  time.Time
+	LowPriority bool
+	Topic       string
+}
+
+func (h *Header) Set(header http.Header) {
+	header.Set(ApnsID, h.ID)
+	header.Set(ApnsExpiration, strconv.FormatInt(h.Expiration.Unix(), 10))
+	header.Set(ApnsPriority, "5")
+	header.Set(ApnsTopic, h.Topic)
+}

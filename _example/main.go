6b237f1bf76ac0b8301ed0d7e7ff19b113e9fd46
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-15 11:31:13 +0200

    refactor

--- _example/main.go
+++ _example/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 
+	"github.com/sger/go-apns2"
 	"github.com/sger/go-apns2/certificate"
-	"github.com/sger/go-apns2/client"
 )
 
 func main() {
@@ -17,9 +13,6 @@ func main() {
 	var deviceToken = "c7800a79efffe8ffc01b280717a936937cb69f8ca307545eb6983c60f12e167a"
 	var filename = "certs/PushChatKey.p12"
 	var password = "pushchat"
-
-	// POST URL
-	url := fmt.Sprintf("%v/3/device/%v", client.Development, deviceToken)
 
 	// Setup payload must contains an aps root label and alert message
 	payload := []byte(`{ "aps" : { "alert" : "Hello world" } }`)
@@ -36,40 +29,11 @@ func main() {
 	}
 
 	// Setup a new http client
-	client, err := client.NewClient(certificate)
+	client, err := apns2.NewClient(certificate)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Sending the request with valid PAYLOAD (must starts with aps)
-	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Send JSON Header
-	// TODO
-	req.Header.Set("Content-Type", "application/json")
-
-	// Do the request
-	resp, err := client.HTTPClient.Do(req)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	// Read the response
-	fmt.Println(resp.Status)
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s\n", string(body))
+	client.Send(payload, deviceToken)
 }

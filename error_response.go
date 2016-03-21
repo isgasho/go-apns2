a60670443110f373db19a3fdd6efd5cd096e2088
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-21 16:23:03 +0200

    .gitignore

--- error_response.go
+++
@@ -1,7 +0,0 @@
-package apns2
-
-// ErrorResponse contains reason, timestamp
-type ErrorResponse struct {
-	Reason    string `json:"reason,omitempty"`
-	Timestamp int64  `json:"timestamp,omitempty"`
-}
